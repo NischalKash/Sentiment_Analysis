adf05bbaaa77a80b34253af639f13ca30beb49cb
Nischal <nkashya@ncsu.edu>
2020-09-07 15:15:57 -0400

    Pushed Pdf to text code

--- sentimental_analysis/realworld/views.py
+++ sentimental_analysis/realworld/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+import sys
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+
+def pdfparser(data):
+
+    fp = open(data, 'rb')
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+        data =  retstr.getvalue()
+
+    text_file = open("Output.txt", "w")
+    text_file.write(data)
+
+    text_file = open("Output.txt",'r')
+    a = ""
+    for x in text_file:
+            if len(x)>2:
+                b = x.split()
+                for i in b:
+                    a += i + " "
+    return a
 
 def analysis(request):
-    note = "Hello"
-    return render(request,'realworld/analysis.html',{'note':note})
+    return render(request,'realworld/analysis.html')
 
 def input(request):
     if request.method=='POST':
@@ -11,6 +42,9 @@ def input(request):
         file = request.FILES['document']
         fs = FileSystemStorage()
         fs.save(file.name,file)
+        pathname = "media/"
+        value = pdfparser(pathname+file.name)
+        print(value)
     else:
         note = "Please Enter the file you want it to be uploaded"
         return render(request, 'realworld/home.html', {'note': note})
@@ -18,7 +52,8 @@ def input(request):
 def productanalysis(request):
     if request.method=='POST':
         blogname = request.POST.get("blogname", "")
-        print(blogname)
+        text_file = open("Output.txt", "w")
+        text_file.write(blogname)
     else:
         note = "Please Enter the product blog name for analysis"
         return render(request, 'realworld/productanalysis.html', {'note': note})
